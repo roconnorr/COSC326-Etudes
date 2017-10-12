4cfefe382bd051df1831d18a3df0483e90d5dbd6
Rory-mbp <roryoconnor@outlook.co.nz>
2017-10-12 15:44:13 +1300

    Removed unnecessary files.

--- 05/print mouse java.java
+++
@@ -1,8 +0,0 @@
-//prints coords at mouse click DEBUG ONLY
-        quiltPanel.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent event) {
-                Point p = event.getPoint();
-                System.out.println(p);
-            }
-        });
